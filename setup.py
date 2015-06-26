88ad03560183835a89ce5cb799baa8a3b451b28a
Jiri Suchan <jiri@tictrac.com>
2015-06-26 13:41:52 +0100

    put deps into setup.py

--- setup.py
+++ setup.py
@@ -41,4 +41,8 @@ setup(
 	long_description=README,
 	url="https://github.com/jleclanche/django-push-notifications",
 	version=push_notifications.__version__,
+	install_requires=[
+		'django-uuidfield',
+		'django',
+	],
 )
